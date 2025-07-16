bd814558047cb3539bd67409e576db537b49c607
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2025-07-16 23:32:11 +0200

    add !! abbreviation to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,4 +10,12 @@ alias vi="vim"
 alias ll="ls -lisah"
 alias la="ls -1A"
 
+# Functions
+function last_history_item
+    echo $history[1]
+end
+
+# Abbreviations
+abbr -a !! --position anywhere --function last_history_item
+
 starship init fish | source
